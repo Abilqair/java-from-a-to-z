2cc5eb5e1bfc030ca648290353693bf6576c9b4f
Abilqair <hurricane_axe@mail.ru>
2016-04-15 19:35:40 +0600

    Update CalcInt.java

--- CalcInt.java
+++ CalcInt.java
@@ -1,12 +1,20 @@
 package frag;
 
+
 /**
- * Created by BAtmAN on 11.03.16.
+ * Created by BAtmAN on 15.04.16.
  */
 public class CalcInt {
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
+        calculator.add(1, 2 );
+        calculator.substract(3, 2);
+        calculator.div(2, 2);
+        calculator.multiple(5, 2 );
+
+
+
 
         calculator.showCalc();
 
@@ -14,3 +22,4 @@ public class CalcInt {
     }
 
 }
+
