73276a1cb6eea2cbd266530545ee84ab776c22d0
Abilqair <hurricane_axe@mail.ru>
2016-04-15 17:29:27 +0600

    add calcint.java

---
+++ CalcInt.java
@@ -0,0 +1,16 @@
+package frag;
+
+/**
+ * Created by BAtmAN on 11.03.16.
+ */
+public class CalcInt {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        calculator.showCalc();
+
+
+    }
+
+}
