e2fe39b3a0fe5bbc16aa9ca8a69bb279b4b5e657
Abilqair <hurricane_axe@mail.ru>
2016-05-15 14:12:04 +0600

    Update TwoDimension.java

--- arraus/TwoDimension.java
+++ arraus/TwoDimension.java
@@ -1,17 +1,22 @@
 package arraus;
 public class TwoDimension {
-    public static void main(String[] args) {
-        int[][] values = {
-                {1,-9,5},
-                {4,2,3},
-                {7,-5,6}
-                 };      
-        int n = values.length;
-        for(int i = n-1;i >=0;i--){
-            for(int j = 0;j < n;j++){
-                System.out.print(values[j][i] + "\t");
-           }
-           System.out.println("");
-        }
-        }
+    private final int[][] values;
+
+public TwoDimension(final int[][] values) {
+this.values = values;
+}
+
+public void rotate() {
+int n =this.values.length;
+for(int i = n-1;i >=0;i--){
+for(int j = 0;j < n;j++){
+System.out.print(this.values[j][i] + "\t");
+}
+System.out.println("");
+}
+}
+
+public static void main(String[] args) {
+new TwoDimension(new int[][] {{1, -9, 5},{4, 2, 3},{7, -5, 6}}).rotate();
+}
 }
