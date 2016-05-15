087b4ec989d657f8c7cc2ce5d72e7d5f0a605195
Abilqair <hurricane_axe@mail.ru>
2016-05-15 14:09:15 +0600

    Update ArrayBubble.java

--- arraus/ArrayBubble.java
+++ arraus/ArrayBubble.java
@@ -1,23 +1,22 @@
 package arraus;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 
 public class ArrayBubble{
+	private int[] values;
 	
-    public static void main(String[] args) throws Exception
-    {
-        BufferedReader reader  = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] values = new int[10];
-        int a, b, c;
-
-        int glavniirazmer = values.length;
-
-        for (int i = 0; i < glavniirazmer; i++)
-        {
-            values[i] = Integer.parseInt(reader.readLine());
-        }
+	
+	
+	public ArrayBubble(int[] values){
+		this.values = values;
+		
+		}
+		
+		public void rotate() {
+			
+		int a, b, c;
+		int glavniirazmer = values.length;
+		
+		
         for (a = 1; a < glavniirazmer; a++)
             for (b = glavniirazmer - 1; b >= a; b--) {
                 if (values[b - 1] > values[b]) {
@@ -28,5 +27,10 @@ public class ArrayBubble{
             }
         for (int i = 0; i < glavniirazmer; i++)
             System.out.println(" " + values[i]);
-    }
+		}
+    
+    
+    public static void main(String[] args) {
+new ArrayBubble(new int[] {1, -9, 5,4, 2, 3,7, -5, 6}).rotate();
+}
 }
