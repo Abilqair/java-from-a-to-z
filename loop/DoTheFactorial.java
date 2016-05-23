14241afe00068554cb83ab12e4fd2b1d8aad1640
Abilqair <hurricane_axe@mail.ru>
2016-05-23 14:54:28 +0600

    Update DoTheFactorial.java

--- loop/DoTheFactorial.java
+++ loop/DoTheFactorial.java
@@ -1,28 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+package loop;
+/** Класс вычисляет факториал
+* @author B.W.
+*@since 23.05.16
+*/
 public class DoTheFactorial {
-    public static void main(String[] args) throws Exception {
-        BufferedReader dance = new BufferedReader(new InputStreamReader(System.in));
-        String n = dance.readLine();
+    private int n;
 
-        int f = Integer.parseInt(n);
-        if(f > 0) {
+    public DoTheFactorial(int n){
+        this.n = n;
+    }
+    public long calculate(){
+        if (n > 0){
             int fact = 1;
-            for (int i = 1; i <= f; i++) {
+            for (int i = 1; i <= n; i++) {
                 fact = fact * i;
-
             }
-            System.out.println("factorial raven = " + fact);
+		  
+            System.out.println("факториал равен = " + fact);
+		  
         }
-        else if (f == 0)
+        else if (n == 0)
         {
-            System.out.println("factorial raven = 1");
-        }else if(f < 0)
-            System.out.println("vvedennoe chislo otricatelnoe");
+            System.out.println("факториал равен = 1");
+        }
+        else if(n < 0){
+            System.out.println("введенное число отрицательное");
+        }
+        return n;
+    }
 
-
+    public static void main(String[] args) {
+        DoTheFactorial crew = new DoTheFactorial(6);
+        crew.calculate();
     }
 }
-//proged by B.W.
