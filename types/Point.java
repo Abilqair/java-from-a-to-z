b8f2ab33ea15720df0de1e875e52e7b8e4204ac2
Abilqair <hurricane_axe@mail.ru>
2016-05-23 14:11:21 +0600

    Update Point.java

--- types/Point.java
+++ types/Point.java
@@ -1,3 +1,9 @@
+package types;
+/** Класс расчитывает точку в системе координат
+* @author B.W.
+*@since 23.05.16
+* @version 1.2
+*/
 public class Point {
     public double x;
     public double y;
@@ -6,15 +12,11 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
+//Метод расчитывает точку в системе координат по x и y
     public double distanceTo(Point point) {
 
         double n = this.x - point.x;
         double m = this.y - point.y;
         return Math.sqrt(n * n + m * m);
-
-
-
-
     }
 }
