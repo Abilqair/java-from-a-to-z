661fd4ede87c29178bfffa856b5905d46fb2ac10
Abilqair <hurricane_axe@mail.ru>
2016-07-29 22:34:49 +0600

    Update Item.java

--- bw/models/Item.java
+++ bw/models/Item.java
@@ -1,24 +1,50 @@
 /** Класс содержит документ заявки
 * @author B.W.
-*@since 08.07.16
+*@since 29.07.16
 */
 package kz.bw.models;
 
 public class Item {
-	public String id;
+	private String id;
 	
-	public String name;
+	private String name;
 	
-	public String description;
+	private String description;
 	
-	public long create;
+	private long create;
+	
+	private String[] comment = new String[10];
+	private int current = 0;
+	
+	
+	public void setComm(String comm){
+		if(this.current == this.comment.length-1){
+			this.current = 0;
+		}
+		this.comment[this.current++] = comm;
+	}
+	
+	public String getComm(){
+		int pos = this.current == 0 ?this.current : this.current  - 1;
+		return this.comment[pos];
+	}
+	
+	public String getComm(int position){
+		return this.comment[position < this.comment.length?position:this.current];
+	}
 	
 	public Item(){}
 	
-	public Item(String name, String description, long create){
+	public Item(String name, String description){
 		this.name = name;
 		this.description = description;
-		this.create = create;
+		
+	}
+	
+	public Item (String id, String name, String description) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
 	}
 	
 	public String getName(){
