1ec64fbf49b3f52c2cf319e9b7593ea96f59eab2
Abilqair <hurricane_axe@mail.ru>
2016-08-11 11:49:19 +0600

    changes

---
+++ CalcInt.java
@@ -0,0 +1,25 @@
+package frag;
+
+
+/**
+ * Created by BAtmAN on 15.04.16.
+ */
+public class CalcInt {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        calculator.add(1, 2 );
+        calculator.substract(3, 2);
+        calculator.div(2, 2);
+        calculator.multiple(5, 2 );
+
+
+
+
+        calculator.showCalc();
+
+
+    }
+
+}
+
